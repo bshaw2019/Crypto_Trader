53ab5f708246dcbe6350df2c6b144e06bdcef5ee
Brandon Shaw <bshaw2012@BSHAW19.local>
2018-03-04 02:37:42 -0500

    Added generate_input_dataset function and a loop to build dataframes for all Poloniex trading pairs

--- Inputs_Table_Builder.py
+++ Inputs_Table_Builder.py
@@ -4,8 +4,8 @@ import numpy as np
 from datetime import datetime, timedelta
 from Variable_Functions.Twitter_Sentiment import dates_to_sentiment
 
-
 db_connection = sqlite3.connect('databases/market_prices.db')
+
 
 
 ######################################## Bid Ask Spread Time #######################################
@@ -14,7 +14,7 @@ def OLHCV_From_DB(symbol):
 	return df
 
 
-######################################### Twitter Sentiment ##########################
+######################################### Twitter Sentiment #######################################
 def twitter_sentiment(symbol, max_tweets):
 
 	now = datetime.now() 
@@ -24,22 +24,33 @@ def twitter_sentiment(symbol, max_tweets):
 
 
 
+######################################### Main function to build input dataset #####################
+def generate_input_dataset(symbols):
+
+	table_names = {}
+
+	for symbol in symbols:
+		table_names[symbol] = OLHCV_From_DB(symbol)
+
+	for symbol in symbols:
+		sentiment_variables = twitter_sentiment(symbol, 25)
+		table_names[symbol]['Tweet_Sentiment_Polarity'] = sentiment_variables[0]
+		table_names[symbol]['Tweet_Sentiment_Subjectivity'] = sentiment_variables[1]
+		table_names[symbol]['Tweet_Positive_Percent'] = sentiment_variables[2]
+		table_names[symbol]['Tweet_Sentiment_STDDEV'] = sentiment_variables[3]
+
+	return table_names
 
 
-table_names = {}
-symbols = ['ETH/BTC', 'LTC/BTC']
 
-for symbol in symbols:
-	table_names[symbol] = OLHCV_From_DB(symbol)
+#Test Case
+cursor = db_connection.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+symbols1 = cursor.fetchall()
 
-for symbol in symbols:
-	sentiment_variables = twitter_sentiment(symbol, 100)
-	table_names[symbol]['Tweet_Sentiment_Polarity'] = sentiment_variables[0]
-	table_names[symbol]['Tweet_Sentiment_Subjectivity'] = sentiment_variables[1]
-	table_names[symbol]['Tweet_Positive_Percent'] = sentiment_variables[2]
-	table_names[symbol]['Tweet_Sentiment_STDDEV'] = sentiment_variables[3]
+symbols = []
+for symbol in symbols1:
+	symbols.append(symbol[0])
 
-for symbol in symbols:
-	print(symbol)
-	print(table_names[symbol])
-
+input_tables = generate_input_dataset(symbols)
+print(input_tables)
--- Variable_Functions/Twitter_Sentiment.py
+++ Variable_Functions/Twitter_Sentiment.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def dates_to_sentiment(dates, ticker, max_tweets):
 
-    ticker = "$" + ticker
+    ticker = ticker
+    print("Calculating sentiment for:" + ticker)
 
     sentiments = []
     positives = []
@@ -14,8 +15,8 @@ def dates_to_sentiment(dates, ticker, max_tweets):
     for d in dates:
         arrow_date = arrow.get(d)
         tweetCriteria = got3.manager.TweetCriteria().setQuerySearch("{}{}".format("#", ticker)).setMaxTweets(max_tweets) \
-            .setSince(arrow_date.format("YYYY-MM-DD")) \
-            .setUntil(arrow_date.replace(days=1).format("YYYY-MM-DD"))
+            .setSince(arrow_date.replace(days=-1).format("YYYY-MM-DD")) \
+            .setUntil(arrow_date.replace(days=+1).format("YYYY-MM-DD"))
         tweets = got3.manager.TweetManager.getTweets(tweetCriteria)
         sents_per_date = []
         subjectivity = []
@@ -34,11 +35,16 @@ def dates_to_sentiment(dates, ticker, max_tweets):
 
         standard_dev_array = np.asarray(sents_per_date)
 
-        mean_polarity = sum(sents_per_date) / len(sents_per_date)
-        mean_subjectivity = sum(subjectivity) / len(sents_per_date)
-        percent_positive = len(positives) / len(sents_per_date)
-        standard_deviation_polarity = np.std(standard_dev_array)
-
+        if len(sents_per_date) >= 1:
+            mean_polarity = sum(sents_per_date) / len(sents_per_date)
+            mean_subjectivity = sum(subjectivity) / len(sents_per_date)
+            percent_positive = len(positives) / len(sents_per_date)
+            standard_deviation_polarity = np.std(standard_dev_array)
+        else:
+            mean_polarity = 0
+            mean_subjectivity = 0
+            percent_positive = .5
+            standard_deviation_polarity = 0
         #Mean Polarity 
         sentiments.append(mean_polarity)
         #Mean Subjectivity 
